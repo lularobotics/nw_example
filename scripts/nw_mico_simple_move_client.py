3e127b1a3799d87c0963127a33b3884974de6dfb
Nathan Ratliff <ratliff.nathan@gmail.com>
2016-02-22 15:03:57 -0800

    Fix some bugs in python client and demo ordering
    - use_upright_orientation_constraint_end_only was referenced as
      use_upright_constraint_end_only; field was always being set to False
      in the action message.
    - Demos used to start with the upright orientation constraint applied to
      the entire length of the trajectory. That's unsafe because if the
      robot isn't currently in a configuration with its end-effector
      "upright", the optimization tries to get it there as fast as possible.
      The safety checks catch the resulting high accelerations, but that
      means optimization fails. Now it's ordered so that it runs through
      upright constraints applied to only the end-effector first, then
      the full trajectory, then not at all. That ordering allows the demos
      to loop through that sequence as many times as needed, and also to
      start from any collision-free configuration.
    - Now it also cycles through all of the approach-shaping variants before
      doing the entire thing again without approach shaping.

--- scripts/nw_mico_simple_move_client.py
+++ scripts/nw_mico_simple_move_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-
-# Note: this client is identical to the c++ version and it can be used in the
-# same way
+#
+# Note: This client is identical to the c++ version and it can be used in the
+# same way.
 
 import sys
 import os
@@ -28,9 +28,10 @@ def CreatePlanningRequest(target_x, target_y, target_z, task_config):
   goal.approach_constraint_csv = task_config['approach_constraint_csv']
   goal.shape_approach = bool(task_config['shape_approach'])
   goal.upright_constraint_direction_csv = task_config['upright_constraint_direction_csv']
-  goal.use_upright_orientation_constraint = bool(task_config['use_upright_orientation_constraint'])
-  goal.use_upright_orientation_constraint_end_only = bool(task_config['use_upright_constraint_end_only'])
-
+  goal.use_upright_orientation_constraint = bool(
+      task_config['use_upright_orientation_constraint'])
+  goal.use_upright_orientation_constraint_end_only = bool(
+      task_config['use_upright_orientation_constraint_end_only'])
   goal.behavioral_type = task_config['behavioral_type']
   goal.obstacle_linearization_constraint_csv = task_config['obstacle_linearization_constraint_csv']
   goal.passthrough_constraint_csv = task_config['passthrough_constraint_csv']
@@ -78,9 +79,9 @@ def main(args):
   rospy.init_node("nw_mico_simple_move_client")
   rospy.loginfo("Running NW MICO simple move client")
 
-  #############################################
-  ## Prepare task config ######################
-  #############################################
+  #----------------------------------------------------------------------------
+  #- Prepare task config ------------------------------------------------------
+  #----------------------------------------------------------------------------
 
   if len(args) != 4:
     rospy.logerr("Invalid number of arguments")
@@ -93,7 +94,7 @@ def main(args):
     'shape_approach' : False,
     'upright_constraint_direction_csv' : "0.,0.,1.",
     'use_upright_orientation_constraint' : False,
-    'use_upright_constraint_end_only' : False, 
+    'use_upright_orientation_constraint_end_only' : False, 
     'behavioral_type' : "",
     'obstacle_linearization_constraint_csv' : "",
     'passthrough_constraint_csv' : ""
@@ -107,15 +108,17 @@ def main(args):
 
   # update the default values with user config
   task_config.update(loaded_task_config)
+  for key,value in sorted(task_config.iteritems()):
+    print key, value
 
   # get target position
   target_x = args[1]
   target_y = args[2]
   target_z = args[3]
 
-  #############################################
-  ## Create planning client ###################
-  #############################################
+  #--------------------------------------------
+  #- Create planning client -------------------
+  #--------------------------------------------
 
   # Setup a client to query a trajectory once planning is complete and publish
   # it to the /joint_trajectory topic immediately on the server side.
@@ -124,9 +127,9 @@ def main(args):
   rospy.loginfo("Waiting for planning server")
   planning_client.wait_for_server()
 
-  #############################################
-  ## Create trajectory service ################
-  #############################################
+  #----------------------------------------------------------------------------
+  #- Create trajectory service ------------------------------------------------
+  #----------------------------------------------------------------------------
 
   # Setup the broadcast request service
   broadcast_trajectory = rospy.ServiceProxy(
@@ -134,9 +137,9 @@ def main(args):
   rospy.loginfo("Waiting for trajectory broadcasting service")
   rospy.wait_for_service(BroadcastTrajectoryRequest.DEFAULT_SERVICE_NAME)
 
-  #############################################
-  ## Create and send planning request #########
-  #############################################
+  #----------------------------------------------------------------------------
+  #- Create and send planning request -----------------------------------------
+  #----------------------------------------------------------------------------
 
   # Send the planning request and wait for planning to complete. 
   planning_request = CreatePlanningRequest(target_x, target_y, target_z, task_config)
@@ -150,14 +153,14 @@ def main(args):
 
   rospy.loginfo("Planning done and succeeded.")
 
-  #############################################
-  ## Reuqest trajectory broadcast #############
-  #############################################
+  #----------------------------------------------------------------------------
+  #- Reuqest trajectory broadcast ---------------------------------------------
+  #----------------------------------------------------------------------------
 
   # Send the trajectory broadcast request and wait for the execution to finish. 
   rospy.loginfo("Sending trajectory broadcasting request")
   time_from_start = 0.0
-  dilation_factor = 1.0
+  dilation_factor = 1.0  # Play this this to speed up (<1) or slow down (>1) execution.
   timing = TrajectoryTiming(time_from_start, dilation_factor)
   srv_result = broadcast_trajectory(trajectory_timing=timing)
 
